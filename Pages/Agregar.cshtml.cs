8cff11926dd4b8db3bedff1d52ed230409417f0c
Gama <e.gamaliel.mc@gmail.com>
2022-01-15 16:21:17 -0600

    Se editan las vistas: Inicio
    Se crean las vistas:
    Agregar

---
+++ Pages/Agregar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrueHomePT.Pages
+{
+    public class AgregarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
